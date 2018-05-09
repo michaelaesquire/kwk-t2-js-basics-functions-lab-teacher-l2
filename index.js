cba425acc14c3a88a10e4d28cda41509ac2cc784
Michaela Olson <mco44@cornell.edu>
2018-05-09 22:00:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(inputValue) {
   let returnValue;
-  if (inputValue == 43) {
-    returnValue = 1;
+
+  
+  if (inputValue >= 42) {
+    returnValue = inputValue - 42;
   }
-  else if (inputValue == 50) {
-    returnValue == 8;
+  else if (inputValue < 42) {
+    returnValue = 42 - inputValue;
   }
-  else if (inputValue == 43) {
-    returnValue == 8;
-  }
+//  else if (inputValue == 34) {
+//    returnValue == 8;
+//  }
   return returnValue;
 }
