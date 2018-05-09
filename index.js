29e7ecfb6bbf0db3472f540ed5ebf1508a9e7bdc
Michaela Olson <mco44@cornell.edu>
2018-05-09 22:01:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(inputValue) {
   let returnValue;
-
-  
   if (inputValue >= 42) {
     returnValue = inputValue - 42;
   }
   else if (inputValue < 42) {
     returnValue = 42 - inputValue;
   }
-//  else if (inputValue == 34) {
-//    returnValue == 8;
-//  }
   return returnValue;
 }
